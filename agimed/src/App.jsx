79fde1c8c2df44308d1b976b2f7c8ea038487baf
Andre Alves Leocadio <andre.leocadio@getrak.com.br>
2022-11-21 20:59:30 -0300

    Adiciona roteamento na aplicação

--- agimed/src/App.jsx
+++ agimed/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import {
+  Route,
+  Routes,
+  useNavigate
+} from "react-router-dom";
+
+
+function P1() {
+  const navigate = useNavigate()
+  return (
+    <div onClick={() => { navigate('/about') }}>
+      Cole mano 1
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Agimed</h1>
-      <p>Hello World</p>
-    </div>
+    <Routes>
+      <Route path='/' element={<P1 />} />
+      <Route path='/about' element={<div>Cole mano 2</div>}/>
+    </Routes>
   )
 }
 
