c11434d78e7089b46c2dc9eee08d87c3d7e10a7f
Andre Alves Leocadio <andre.leocadio@getrak.com.br>
2022-12-05 20:51:34 -0300

    Validações necessárias no cadastro de usuarios

--- src/Pages/CadastroUsuario/index.jsx
+++ src/Pages/CadastroUsuario/index.jsx
@@ -6,6 +6,7 @@ import '../global.css'
 import arrowLeftImg from '../../assets/arrow-left.svg'
 import { useFormik } from "formik";
 import { useEffect } from "react";
+import swal from "sweetalert";
 
 
 export default function () {
@@ -15,8 +16,11 @@ export default function () {
       nomeCompleto: '',
       cpf: '',
       cep: '',
+      numero: '',
+      complemento: '',
       endereco: '',
-
+      
+      email: '',
       senha: '',
       confirmacaoSenha: '',
 
@@ -29,6 +33,120 @@ export default function () {
   })
 
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const {cep} = formik.values
+    if(!cep) {
+      return
+    }
+
+    const onlyNumbers = cep.replace(/\D/g, '')
+    formik.setFieldValue('cep', onlyNumbers)
+  }, [formik.values.cep])
+
+
+  async function handleSearchCep() {
+    const {cep} = formik.values
+    if(!cep) {
+      return
+    }
+    const url = `https://brasilapi.com.br/api/cep/v2/${cep}`
+    
+    let request = await fetch(url)
+    if(request.status !== 200) {
+      formik.setFieldValue('endereco', '')
+      formik.setFieldValue('cep', '')
+      swal({
+        icon: 'error',
+        text: 'Cep inválido'
+      })
+      return
+    }
+
+    const r = await request.json()
+    const address = `${r?.street}, ${r?.neighborhood} - ${r?.city}/${r?.state}`
+    formik.setFieldValue('endereco', address)
+  }
+
+
+  function validarCPF(valor) {
+    let cpf = String(valor)
+    cpf = cpf.replace(/[^\d]+/g,'');	
+    if(cpf == '') return false;	
+    // Elimina CPFs invalidos conhecidos	
+    if (cpf.length != 11 || 
+      cpf == "00000000000" || 
+      cpf == "11111111111" || 
+      cpf == "22222222222" || 
+      cpf == "33333333333" || 
+      cpf == "44444444444" || 
+      cpf == "55555555555" || 
+      cpf == "66666666666" || 
+      cpf == "77777777777" || 
+      cpf == "88888888888" || 
+      cpf == "99999999999")
+        return false;		
+    // Valida 1o digito	
+    let add = 0;	
+    for (let i=0; i < 9; i ++)		
+      add += parseInt(cpf.charAt(i)) * (10 - i);	
+      let rev = 11 - (add % 11);	
+      if (rev == 10 || rev == 11)		
+        rev = 0;	
+      if (rev != parseInt(cpf.charAt(9)))		
+        return false;		
+    // Valida 2o digito	
+    add = 0;	
+    for (let i = 0; i < 10; i ++)		
+      add += parseInt(cpf.charAt(i)) * (11 - i);	
+    rev = 11 - (add % 11);	
+    if (rev == 10 || rev == 11)	
+      rev = 0;	
+    if (rev != parseInt(cpf.charAt(10)))
+      return false;		
+    return true;   
+  }
+
+
+  function handleValidCpf() {
+    const {cpf} = formik.values
+    if(!cpf) {
+      return
+    }
+
+    const onlyNumbers = cpf.replace(/\D/g, '')
+    if(!validarCPF(onlyNumbers)) {
+      swal({
+        icon: 'error',
+        text: 'CPF inválido'
+      })
+      formik.setFieldValue('cpf', '')
+      return
+    }
+  }
+
+  useEffect(() => {
+    const {cpf} = formik.values
+    if(!cpf) {
+      return
+    }
+
+    const onlyNumbers = cpf.replace(/\D/g, '')
+
+    const pt1 = onlyNumbers.slice(0, 3)
+    const pt2 = onlyNumbers.slice(3, 6)
+    const pt3 = onlyNumbers.slice(6, 9)
+    const pt4 = onlyNumbers.slice(9, 11)
+
+    let format = `${pt1}`
+    format += pt2 ? `.${pt2}`: ''
+    format += pt3 ? `.${pt3}`: ''
+    format += pt4 ? `-${pt4}`: ''
+
+    formik.setFieldValue('cpf', format)
+  }, [formik.values.cpf])
+
+  
   
   return (
     <Container className='align-items-center mt-5'>
@@ -54,15 +172,35 @@ export default function () {
           </Form.Group>
           <Form.Group className="mb-3" controlId="cadastroCPF">
             <Form.Label className='color-custom-primary'>CPF</Form.Label>
-            <Form.Control required className='input-custom-primary' type="text" onChange={formik.handleChange} value={formik.values.cpf} name='cpf' />
+            <Form.Control required className='input-custom-primary' type="text" onChange={formik.handleChange} value={formik.values.cpf} name='cpf' onBlur={handleValidCpf} />
           </Form.Group>
 
           <hr className='mt-5 mb-5' />
           
-          <Form.Group className="mb-3" controlId="cadastroCEP">
-            <Form.Label className='color-custom-primary'>CEP</Form.Label>
-            <Form.Control required className='input-custom-primary' type="number" onChange={formik.handleChange} value={formik.values.cep} name='cep' />
-          </Form.Group>
+          <Row>
+            <Col sm={12} md={3}>
+              <Form.Group className="mb-3" controlId="cadastroCEP">
+                <Form.Label className='color-custom-primary'>CEP</Form.Label>
+                <Form.Control onBlur={handleSearchCep} required className='input-custom-primary' type="text" onChange={formik.handleChange} value={formik.values.cep} name='cep' />
+              </Form.Group>
+            </Col>
+            <Col></Col>
+          </Row>
+
+          <Row>
+            <Col sm={12} md={6}>
+              <Form.Group className="mb-3" controlId="cadastroNumero">
+                <Form.Label className='color-custom-primary'>Número</Form.Label>
+                <Form.Control required className='input-custom-primary' type="text" onChange={formik.handleChange} value={formik.values.numero} name='numero' />
+              </Form.Group>
+            </Col>
+            <Col sm={12} md={6}>
+              <Form.Group className="mb-3" controlId="cadastroComplemento">
+                <Form.Label className='color-custom-primary'>Complemento</Form.Label>
+                <Form.Control className='input-custom-primary' type="text" onChange={formik.handleChange} value={formik.values.complemento} name='complemento' />
+              </Form.Group>
+            </Col>
+          </Row>
           <Form.Group className="mb-3" controlId="cadastroEndereco">
             <Form.Label className='color-custom-primary'>Endereço</Form.Label>
             <Form.Control required className='input-custom-primary' readOnly type="text" onChange={formik.handleChange} value={formik.values.endereco} name='endereco' />
@@ -71,14 +209,25 @@ export default function () {
           <hr className='mt-5 mb-5' />
 
 
-          <Form.Group className="mb-3" controlId="cadastroSenha">
-            <Form.Label className='color-custom-primary'>Senha</Form.Label>
-            <Form.Control required className='input-custom-primary' type="password" onChange={formik.handleChange} value={formik.values.senha} name='senha' />
+          <Form.Group className="mb-3" controlId="cadastroEmail">
+            <Form.Label className='color-custom-primary'>E-mail</Form.Label>
+            <Form.Control required className='input-custom-primary' type="email" onChange={formik.handleChange} value={formik.values.email} name='email' />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="cadastroSenhaConfirmacao">
-            <Form.Label className='color-custom-primary'>Confirmação da Senha</Form.Label>
-            <Form.Control required className='input-custom-primary' type="password" onChange={formik.handleChange} value={formik.values.confirmacaoSenha} name='confirmacaoSenha' />
-          </Form.Group>
+          <Row className={formik.values.senha !== formik.values.confirmacaoSenha ? 'border border-danger rounded p-3' : ''}>
+            {formik.values.senha !== formik.values.confirmacaoSenha ? <span className='text-danger'>Senhas não são iguais</span>: null}
+            <Col sm={12} md={6}>
+              <Form.Group className="mb-3" controlId="cadastroSenha">
+                <Form.Label className='color-custom-primary'>Senha</Form.Label>
+                <Form.Control required className='input-custom-primary' type="password" onChange={formik.handleChange} value={formik.values.senha} name='senha' />
+              </Form.Group>
+            </Col>
+            <Col sm={12} md={6}>
+              <Form.Group className="mb-3" controlId="cadastroSenhaConfirmacao">
+                <Form.Label className='color-custom-primary'>Confirmação da Senha</Form.Label>
+                <Form.Control required className='input-custom-primary' type="password" onChange={formik.handleChange} value={formik.values.confirmacaoSenha} name='confirmacaoSenha' />
+              </Form.Group>
+            </Col>
+          </Row>
 
           <hr className='mt-5 mb-5' />
 
