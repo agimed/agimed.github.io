9ebec036a54241f05e4c35a3129fda2e5f84dc06
Andre Alves Leocadio <andre.leocadio@getrak.com.br>
2022-12-06 08:26:46 -0300

    Adiciona opçao de enviar arquivos

--- src/Pages/Mensagens/index.jsx
+++ src/Pages/Mensagens/index.jsx
@@ -2,6 +2,7 @@ import { Button, Col, Container, Row, Form, Modal } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { BiMessageRoundedAdd, BiMessageAltError, BiUser } from 'react-icons/bi'
 import { AiOutlineSend } from 'react-icons/ai'
+import { BsCardImage } from 'react-icons/bs'
 
 import '../global.css'
 import { useState, useRef } from "react";
@@ -10,12 +11,35 @@ import { useEffect } from "react";
 export default function () {
   const navigate = useNavigate()
   const endOfPageComponent = useRef(null)
+  const inputFile = useRef(null)
+  const [file, setFile] = useState(null)
+  
 
   useEffect(() => {
     if(endOfPageComponent.current) {
       endOfPageComponent.current.scrollIntoView({ behavior: 'smooth' })
     }
   }, [endOfPageComponent.current])
+
+  function loadFile(event) {
+    const file = event.target.files[0]
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = function() {
+      const content = reader.result.toString()
+      setFile(content)
+      // enviar o arquivo aqui
+      event.target.value = ''
+      console.log(content)
+    }
+  }
+
+  useEffect(() => {
+    if(inputFile.current) {
+      inputFile.current.addEventListener('change', loadFile)
+      return () => inputFile.current?.removeEventListener('change', loadFile)
+    }
+  }, [inputFile.current])
 
   return (
     <>
@@ -68,6 +92,12 @@ export default function () {
       <div className="end-of-page" />
       <div className='fixed-bottom'>
         <div className="caixa-barra-mensagem mb-1">
+          <input type="file" name="file" id="file" style={{display: 'none'}} ref={inputFile} />
+          <Button variant='custom-secondary' onClick={() => {
+            inputFile.current?.click()
+          }}>
+            <BsCardImage size={25} />
+          </Button>
           <textarea rows={2} className='input-custom-primary p-1 rounded text'  type="text" />
           <Button variant='custom-secondary'>
             <AiOutlineSend />
